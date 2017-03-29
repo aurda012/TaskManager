54e53f0e54fe486e188a0571835836212251dbcf
Alfredo <alfredo.i.urdaneta@gmail.com>
2017-03-29 07:25:58 -0400

    Modified index and started show view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -17,10 +21,15 @@ class ItemsController < ApplicationController
     end
   end
 
+
+
   private
 
   def item_params
-    (params.require(:item).permit(:title, :description)
+    params.require(:item).permit(:title, :description)
   end
 
+  def find_item
+    @item = Item.find(params[:id])
+  end
 end
